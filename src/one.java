a0030889a1558a20975f0c5251094be6cc036ef7
vishu-yadav <vy.vishuyadav09@gmail.com>
2019-01-14 08:53:39 +0530

    added files

---
+++ src/one.java
@@ -0,0 +1,9 @@
+class One
+{
+public static void main(String args[])
+	{
+		int num1=35,num2=45;
+		System.out.println("addition of two integers num1 and num2 are: ");
+		System.out.println(num1+num2);
+	}
+}
---
+++ src/two.java
@@ -0,0 +1,10 @@
+class Two
+{
+	public static void main(String args[])
+	{
+		char C='C';
+		int i='C';
+		System.out.println("Integer representation of the entered character is:");
+		System.out.println(i);
+	}
+}
